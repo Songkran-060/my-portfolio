dcab77dbdb48ae192e4a9a82e3a6774aee556669
Pongpichet Tanthanawikrai <pongpichet.tan@innovasive.co.th>
2024-03-25 22:34:05 +0700

    💦 Add Profile Image

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="flex w-full p-12 justify-center items-center">
+    <div className="flex flex-col gap-y-[16px] w-full p-12 justify-center items-center">
       <p className='text-3xl Header'>Pongpichet's Portfolio</p>
+      <img src='/images/profile.jpg' alt="Songkran's Profile" className='w-[200px] h-[200px] rounded-full border-8 border-black hover:rotate-180 transform duration-1000' />
     </div>
   );
 }
